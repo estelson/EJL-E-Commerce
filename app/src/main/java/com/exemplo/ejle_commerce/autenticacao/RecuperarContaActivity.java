2567e5620ec145c46e38889d19682569100a1dd0
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-10 19:50:24 -0300

    368. Programando Activity de Recuperação de Senha

--- app/src/main/java/com/exemplo/ejle_commerce/autenticacao/RecuperarContaActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/autenticacao/RecuperarContaActivity.java
@@ -1,10 +1,13 @@
 package com.exemplo.ejle_commerce.autenticacao;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.exemplo.ejle_commerce.databinding.ActivityRecuperarContaBinding;
+import com.exemplo.ejle_commerce.helper.FirebaseHelper;
 
 public class RecuperarContaActivity extends AppCompatActivity {
 
@@ -17,6 +20,32 @@ public class RecuperarContaActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         configClicks();
+    }
+
+    public void validarDados(View view) {
+        String email = binding.edtEmail.getText().toString().trim();
+
+        if(!email.isEmpty()) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+
+            recuperarConta(email);
+        } else {
+            binding.edtEmail.requestFocus();
+            binding.edtEmail.setError("Informe o e-Mail");
+        }
+    }
+
+    private void recuperarConta(String email) {
+        FirebaseHelper.getAuth().sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(this, "E-Mail de recuperação de senha enviado para " + email, Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this, FirebaseHelper.validaErros(task.getException().getMessage()), Toast.LENGTH_LONG).show();
+                    }
+
+                    binding.progressBar.setVisibility(View.GONE);
+                });
     }
 
     private void configClicks() {
