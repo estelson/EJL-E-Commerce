3a53e01065142ca4527ec86fccf3deb7283c4b10
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-11 13:21:39 -0300

    372. Configurando Bottom Navigation View da Loja

--- app/src/main/java/com/exemplo/ejle_commerce/activity/app/SplashActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/app/SplashActivity.java
@@ -4,10 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.exemplo.ejle_commerce.activity.loja.MainActivityEmpresa;
 import com.exemplo.ejle_commerce.activity.usuario.MainActivityUsuario;
 import com.exemplo.ejle_commerce.R;
+import com.exemplo.ejle_commerce.helper.FirebaseHelper;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,10 +23,43 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler(getMainLooper()).postDelayed(() -> {
+//        new Handler(getMainLooper()).postDelayed(() -> {
+//            verificarAcesso();
+//        }, 3000);
+        new Handler(getMainLooper()).postDelayed(this::verificarAcesso, 3000);
+    }
+
+    private void verificarAcesso() {
+        if(FirebaseHelper.getAutenticado()) {
+            recuperarAcesso();
+        } else {
             finish();
             startActivity(new Intent(this, MainActivityUsuario.class));
-        }, 3000);
+        }
+    }
+
+    private void recuperarAcesso() {
+        DatabaseReference usuarioRef = FirebaseHelper.getDatabaseReference()
+                .child("usuarios")
+                .child(FirebaseHelper.getIdFirebase());
+
+        usuarioRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                finish();
+
+                if(snapshot.exists()) { // Se for usuário...
+                    startActivity(new Intent(getBaseContext(), MainActivityUsuario.class));
+                } else { // Se for a loja
+                    startActivity(new Intent(getBaseContext(), MainActivityEmpresa.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/exemplo/ejle_commerce/activity/loja/MainActivityEmpresa.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/loja/MainActivityEmpresa.java
@@ -1,6 +1,9 @@
 package com.exemplo.ejle_commerce.activity.loja;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -16,6 +19,10 @@ public class MainActivityEmpresa extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainEmpresaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
     }
 
 }
