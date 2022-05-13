df6c530aaaf7903fda5b28601f9e9511ae9760a7
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-12 22:29:37 -0300

    385. Carregando Imagem da Câmera do Dispositivo

--- app/src/main/java/com/exemplo/ejle_commerce/activity/loja/LojaFormProdutoActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/loja/LojaFormProdutoActivity.java
@@ -7,6 +7,7 @@ import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.exemplo.ejle_commerce.R;
 import com.exemplo.ejle_commerce.databinding.ActivityLojaFormProdutoBinding;
@@ -23,6 +25,10 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.normal.TedPermission;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class LojaFormProdutoActivity extends AppCompatActivity {
@@ -30,6 +36,8 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
     private ActivityLojaFormProdutoBinding binding;
 
     private int resultCode = 0;
+
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,13 +73,11 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
 
         sheetBinding.btnCamera.setOnClickListener(v -> {
             verificarPermissaoCamera();
-
             bottomSheetDialog.dismiss();
         });
 
         sheetBinding.btnGaleria.setOnClickListener(v -> {
             verificarPermissaoGaleria();
-
             bottomSheetDialog.dismiss();
         });
 
@@ -95,7 +101,7 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
 
         showDialogPermissao(
                 permissionListener,
-                new String[] {
+                new String[]{
                         Manifest.permission.CAMERA,
                 },
                 "Você negou a permissão para acessar a câmera do dispositivo. Deseja permitir?"
@@ -117,7 +123,7 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
 
         showDialogPermissao(
                 permissionListener,
-                new String[] {
+                new String[]{
                         Manifest.permission.READ_EXTERNAL_STORAGE,
                 },
                 "Você negou a permissão para acessar a galeria do dispositivo. Deseja permitir?"
@@ -135,8 +141,58 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
                 .check();
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
     private void abrirCamera() {
-        Toast.makeText(this, "Abriu câmera", Toast.LENGTH_SHORT).show();
+        switch (resultCode) {
+            case 0:
+                resultCode = 3;
+                break;
+            case 1:
+                resultCode = 4;
+                break;
+            case 2:
+                resultCode = 5;
+                break;
+        }
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Create the File where the photo should go
+        File photoFile = null;
+
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            Toast.makeText(this, "Erro ao capturar a imagem da câmera do dispositivo. Motivo: " + ex.getMessage().toString(), Toast.LENGTH_LONG).show();
+        }
+
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.exemplo.ejle_commerce.fileprovider",
+                    photoFile);
+
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+            resultLauncher.launch(takePictureIntent);
+        }
     }
 
     private void abrirGaleria() {
@@ -146,13 +202,12 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
 
     private final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(), result -> {
-                if(result.getResultCode() == RESULT_OK) {
-                    // Recupera o caminho da imagem
-                    Uri imagemSelecionada = result.getData().getData();
+                if (result.getResultCode() == RESULT_OK) {
                     String caminhoImagem;
 
-//                    try {
-//                        caminhoImagem = imagemSelecionada.toString();
+                    if(resultCode <= 2) { // Galeria
+                        // Recupera o caminho da imagem
+                        Uri imagemSelecionada = result.getData().getData();
 
                         switch (resultCode) {
                             case 0:
@@ -171,9 +226,28 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
 
                                 break;
                         }
-//                    } catch(Exception e) {
-//                        Toast.makeText(getBaseContext(), "Erro ao carregar imagem da galeria. Motivo: " + e.getMessage(), Toast.LENGTH_LONG).show();
-//                    }
+                    } else { // Câmera
+                        File file = new File(currentPhotoPath);
+                        caminhoImagem = String.valueOf(file.toURI());
+
+                        switch (resultCode) {
+                            case 3:
+                                binding.imagemProduto0.setImageURI(Uri.fromFile(file));
+                                binding.imagemProduto0Fake.setVisibility(View.GONE);
+
+                                break;
+                            case 4:
+                                binding.imagemProduto1.setImageURI(Uri.fromFile(file));
+                                binding.imagemProduto1Fake.setVisibility(View.GONE);
+
+                                break;
+                            case 5:
+                                binding.imagemProduto2.setImageURI(Uri.fromFile(file));
+                                binding.imagemProduto2Fake.setVisibility(View.GONE);
+
+                                break;
+                        }
+                    }
                 }
             }
     );
@@ -182,7 +256,7 @@ public class LojaFormProdutoActivity extends AppCompatActivity {
         Bitmap bitmap = null;
 
         try {
-            if(Build.VERSION.SDK_INT < 28) {
+            if (Build.VERSION.SDK_INT < 28) {
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), caminhoUri);
             } else {
                 ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), caminhoUri);
