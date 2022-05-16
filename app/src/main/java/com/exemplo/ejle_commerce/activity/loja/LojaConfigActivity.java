58392a6432baf046e7dd12ae1999f4091a1a4467
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-16 15:35:03 -0300

    410. Carregando Logo do APP da Galeria do Dispositivo

--- app/src/main/java/com/exemplo/ejle_commerce/activity/loja/LojaConfigActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/loja/LojaConfigActivity.java
@@ -1,16 +1,112 @@
 package com.exemplo.ejle_commerce.activity.loja;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
 
 import com.exemplo.ejle_commerce.R;
+import com.exemplo.ejle_commerce.databinding.ActivityLojaConfigBinding;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.io.File;
+import java.util.List;
 
 public class LojaConfigActivity extends AppCompatActivity {
+
+    private ActivityLojaConfigBinding binding;
+
+    private String caminhoImagem = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loja_config);
+        binding = ActivityLojaConfigBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        configClicks();
     }
+
+    private void configClicks() {
+        binding.imgLogo.setOnClickListener(v -> {
+            verificarPermissaoGaleria();
+        });
+    }
+
+    private void verificarPermissaoGaleria() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                abrirGaleria();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(getBaseContext(), "Permissao negada", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        showDialogPermissao(
+                permissionListener,
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                },
+                "Você negou a permissão para acessar a galeria do dispositivo. Deseja permitir?"
+        );
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        resultLauncher.launch(intent);
+    }
+
+    private void showDialogPermissao(PermissionListener listener, String[] permissoes, String msg) {
+        TedPermission.create()
+                .setPermissionListener(listener)
+                .setDeniedTitle("Permissões negadas")
+                .setDeniedMessage(msg)
+                .setDeniedCloseButtonText("Não")
+                .setGotoSettingButtonText("Sim")
+                .setPermissions(permissoes)
+                .check();
+    }
+
+    private final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Uri imagemSelecionada = result.getData().getData();
+                    caminhoImagem = imagemSelecionada.toString();
+                    binding.imgLogo.setImageBitmap(getBitmap(imagemSelecionada));
+                }
+            }
+    );
+
+    private Bitmap getBitmap(Uri caminhoUri) {
+        Bitmap bitmap = null;
+
+        try {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), caminhoUri);
+            } else {
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), caminhoUri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            }
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), "Erro ao carregar imagem da galeria. Motivo: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        return bitmap;
+    }
+
 }
--- app/src/main/java/com/exemplo/ejle_commerce/fragment/loja/LojaConfigFragment.java
+++ app/src/main/java/com/exemplo/ejle_commerce/fragment/loja/LojaConfigFragment.java
@@ -1,20 +1,44 @@
 package com.exemplo.ejle_commerce.fragment.loja;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.exemplo.ejle_commerce.R;
+import com.exemplo.ejle_commerce.activity.loja.LojaConfigActivity;
+import com.exemplo.ejle_commerce.databinding.FragmentLojaConfigBinding;
 
 public class LojaConfigFragment extends Fragment {
 
+    private FragmentLojaConfigBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loja_config, container, false);
+        binding = FragmentLojaConfigBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        configClicks();
+    }
+
+    private void configClicks() {
+        binding.btnConfigLoja.setOnClickListener(v -> {
+            startActivity(LojaConfigActivity.class);
+        });
+    }
+
+    private void startActivity(Class<?> clazz) {
+        startActivity(new Intent(requireContext(), clazz));
     }
 
 }
