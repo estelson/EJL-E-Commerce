92536adfc9074ee6a4c01f60325a8fe529fb95a7
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-16 23:09:05 -0300

    416. Gerando Public Key e Acess Token do Mercado Pago Parte 1

--- app/src/main/java/com/exemplo/ejle_commerce/activity/loja/LojaRecebimentosActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/loja/LojaRecebimentosActivity.java
@@ -1,16 +1,55 @@
 package com.exemplo.ejle_commerce.activity.loja;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.exemplo.ejle_commerce.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.exemplo.ejle_commerce.databinding.ActivityLojaRecebimentosBinding;
 
 public class LojaRecebimentosActivity extends AppCompatActivity {
+
+    private ActivityLojaRecebimentosBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loja_recebimentos);
+        binding = ActivityLojaRecebimentosBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        configClicks();
     }
+
+    private void configClicks() {
+        binding.include.include.ibVoltar.setOnClickListener(v -> {
+            finish();
+        });
+
+        binding.include.textTitulo.setText("Configs. de recebimentos");
+
+        validarDados();
+    }
+
+    private void validarDados() {
+        String publicKey = binding.edtPublicKey.getText().toString().trim();
+        String accessToken = binding.edtAccessToken.getText().toString().trim();
+        int parcelas = Integer.parseInt(binding.edtQtdeParcelas.getText().toString().trim());
+
+        if(!publicKey.isEmpty()) {
+            if(!accessToken.isEmpty()) {
+                if(parcelas > 0 && parcelas <= 12) {
+
+                } else {
+                    binding.edtQtdeParcelas.requestFocus();
+                    binding.edtQtdeParcelas.setError("Informe uma quantidade de parcelas entre 1 e 12");
+                }
+            } else {
+                binding.edtAccessToken.requestFocus();
+                binding.edtAccessToken.setError("Informe o Access Token");
+            }
+        } else {
+            binding.edtPublicKey.requestFocus();
+            binding.edtPublicKey.setError("Informe a Public Key");
+        }
+    }
+
 }
