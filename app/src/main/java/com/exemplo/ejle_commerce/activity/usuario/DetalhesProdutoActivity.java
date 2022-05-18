8756976cbf00be7e689a3c5a2f307e10ba2e66bf
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-18 13:33:53 -0300

    430. Exibindo Imagens do Produto no Carrossel

--- app/src/main/java/com/exemplo/ejle_commerce/activity/usuario/DetalhesProdutoActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/usuario/DetalhesProdutoActivity.java
@@ -1,16 +1,42 @@
 package com.exemplo.ejle_commerce.activity.usuario;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.exemplo.ejle_commerce.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.exemplo.ejle_commerce.adapter.SliderAdapter;
+import com.exemplo.ejle_commerce.databinding.ActivityDetalhesProdutoBinding;
+import com.exemplo.ejle_commerce.model.Produto;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
 
 public class DetalhesProdutoActivity extends AppCompatActivity {
+
+    private ActivityDetalhesProdutoBinding binding;
+
+    private Produto produto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalhes_produto);
+        binding = ActivityDetalhesProdutoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getExtra();
     }
+
+    private void getExtra() {
+        produto = (Produto) getIntent().getSerializableExtra("produtoSelecionado");
+
+        configDados();
+    }
+
+    private void configDados() {
+        binding.sliderView.setSliderAdapter(new SliderAdapter(produto.getUrlsImagens()));
+        binding.sliderView.startAutoCycle();
+        binding.sliderView.setScrollTimeInSec(4);
+        binding.sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        binding.sliderView.setSliderTransformAnimation(SliderAnimations.FADETRANSFORMATION);
+    }
+
 }
--- app/src/main/java/com/exemplo/ejle_commerce/fragment/usuario/UsuarioHomeFragment.java
+++ app/src/main/java/com/exemplo/ejle_commerce/fragment/usuario/UsuarioHomeFragment.java
@@ -1,5 +1,6 @@
 package com.exemplo.ejle_commerce.fragment.usuario;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.exemplo.ejle_commerce.R;
+import com.exemplo.ejle_commerce.activity.usuario.DetalhesProdutoActivity;
 import com.exemplo.ejle_commerce.adapter.CategoriaAdapter;
 import com.exemplo.ejle_commerce.adapter.LojaProdutoAdapter;
 import com.exemplo.ejle_commerce.databinding.FragmentUsuarioHomeBinding;
@@ -187,7 +189,10 @@ public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.On
 
     @Override
     public void onClick(Produto produto) {
+        Intent intent = new Intent(requireContext(), DetalhesProdutoActivity.class);
+        intent.putExtra("produtoSelecionado", produto);
 
+        startActivity(intent);
     }
 
     @Override
