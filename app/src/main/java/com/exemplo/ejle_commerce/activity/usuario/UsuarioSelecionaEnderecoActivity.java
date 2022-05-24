f07a6faf4aab8dc3718b8ce76de0b594afc4ebb5
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-23 21:07:17 -0300

    465. Criando Adapter Listagem de Seleção de Endereços

--- app/src/main/java/com/exemplo/ejle_commerce/activity/usuario/UsuarioSelecionaEnderecoActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/usuario/UsuarioSelecionaEnderecoActivity.java
@@ -1,14 +1,25 @@
 package com.exemplo.ejle_commerce.activity.usuario;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.exemplo.ejle_commerce.adapter.EnderecoSelecaoAdapter;
 import com.exemplo.ejle_commerce.databinding.ActivityUsuarioSelecionaEnderecoBinding;
+import com.exemplo.ejle_commerce.model.Endereco;
 
-public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity implements EnderecoSelecaoAdapter.OnCLickListener {
 
     private ActivityUsuarioSelecionaEnderecoBinding binding;
+
+    private EnderecoSelecaoAdapter enderecoSelecaoAdapter;
+
+    private final List<Endereco> enderecoList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,17 @@ public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity {
         iniciarComponentes();
 
         configClicks();
+
+        configRv();
+    }
+
+    private void configRv() {
+        binding.rvEnderecos.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvEnderecos.setHasFixedSize(true);
+
+        enderecoSelecaoAdapter = new EnderecoSelecaoAdapter(enderecoList, this);
+
+        binding.rvEnderecos.setAdapter(enderecoSelecaoAdapter);
     }
 
     private void configClicks() {
@@ -29,6 +51,11 @@ public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity {
 
     private void iniciarComponentes() {
         binding.include.textTitulo.setText("Endereço de entrega");
+    }
+
+    @Override
+    public void onClick(Endereco endereco) {
+        Toast.makeText(this, endereco.getNomeEndereco(), Toast.LENGTH_SHORT).show();
     }
 
 }
