3237034f5e4a4a87f77467511ac803ec2c5b0d83
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-28 12:41:14 -0300

    490. Criando Adapter Listagem dos Pedidos da Loja

--- app/src/main/java/com/exemplo/ejle_commerce/adapter/LojaPedidosAdapter.java
+++ app/src/main/java/com/exemplo/ejle_commerce/adapter/LojaPedidosAdapter.java
@@ -13,13 +13,19 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.exemplo.ejle_commerce.R;
+import com.exemplo.ejle_commerce.helper.FirebaseHelper;
 import com.exemplo.ejle_commerce.model.Pedido;
 import com.exemplo.ejle_commerce.model.StatusPedido;
+import com.exemplo.ejle_commerce.model.Usuario;
 import com.exemplo.ejle_commerce.util.GetMask;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
-public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAdapter.MyViewHolder> {
+public class LojaPedidosAdapter extends RecyclerView.Adapter<LojaPedidosAdapter.MyViewHolder> {
 
     private final List<Pedido> pedidoList;
 
@@ -27,7 +33,7 @@ public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAd
 
     private final OnClickListener clickListener;
 
-    public UsuarioPedidosAdapter(List<Pedido> pedidoList, Context context, OnClickListener clickListener) {
+    public LojaPedidosAdapter(List<Pedido> pedidoList, Context context, OnClickListener clickListener) {
         this.pedidoList = pedidoList;
         this.context = context;
         this.clickListener = clickListener;
@@ -36,7 +42,7 @@ public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAd
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_usuario_pedido_adapter, parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loja_pedido_adapter, parent, false);
 
         return new MyViewHolder(itemView);
     }
@@ -45,12 +51,18 @@ public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAd
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Pedido pedido = pedidoList.get(position);
 
+        recuperarCliente(pedido, holder);
+
         holder.textIdPedido.setText(pedido.getId());
         holder.textTotalPedido.setText(context.getString(R.string.valor, GetMask.getValor(pedido.getTotal())));
         holder.textDataPedido.setText(GetMask.getDate(pedido.getDataPedido(), 2));
 
         holder.btnDetalhesPedido.setOnClickListener(v -> {
-            clickListener.onClick(pedido);
+            clickListener.onClick(pedido, "detalhes");
+        });
+
+        holder.btnStatusPedido.setOnClickListener(v -> {
+            clickListener.onClick(pedido, "status");
         });
 
         switch (pedido.getStatusPedido()) {
@@ -68,13 +80,37 @@ public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAd
         holder.textStatusPedido.setText(StatusPedido.getStatus(pedido.getStatusPedido()));
     }
 
+    public void recuperarCliente(Pedido pedido, MyViewHolder holder) {
+        DatabaseReference usuarioRef = FirebaseHelper.getDatabaseReference()
+                .child("usuarios")
+                .child(pedido.getIdCliente());
+
+        usuarioRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    Usuario usuario = snapshot.getValue(Usuario.class);
+                    assert usuario != null;
+                    holder.textNomeCliente.setText(usuario.getNome());
+                } else {
+                    holder.textNomeCliente.setText("Usuário não encontrado");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     @Override
     public int getItemCount() {
         return pedidoList.size();
     }
 
     public interface OnClickListener {
-        void onClick(Pedido pedido);
+        void onClick(Pedido pedido, String operacao);
     }
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
@@ -82,8 +118,10 @@ public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAd
         TextView textStatusPedido;
         TextView textTotalPedido;
         TextView textDataPedido;
+        TextView textNomeCliente;
 
         Button btnDetalhesPedido;
+        Button btnStatusPedido;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -92,8 +130,10 @@ public class UsuarioPedidosAdapter extends RecyclerView.Adapter<UsuarioPedidosAd
             textStatusPedido = itemView.findViewById(R.id.textStatusPedido);
             textTotalPedido = itemView.findViewById(R.id.textTotalPedido);
             textDataPedido = itemView.findViewById(R.id.textDataPedido);
+            textNomeCliente = itemView.findViewById(R.id.textNomeCliente);
 
             btnDetalhesPedido = itemView.findViewById(R.id.btnDetalhesPedido);
+            btnStatusPedido = itemView.findViewById(R.id.btnStatusPedido);
         }
     }
 
