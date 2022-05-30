7befb04a70ede184b9fc0da83ab1becb9dcdac10
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-30 19:55:03 -0300

    507. Efetuando o Pagamento do Pedido pelo APP - Parte 2
    Confogurando o método para criar o JSON dos dadso da compra a ser enviado ao MercadoPago

--- app/src/main/java/com/exemplo/ejle_commerce/activity/usuario/UsuarioPagamentoPedidoActivity.java
+++ app/src/main/java/com/exemplo/ejle_commerce/activity/usuario/UsuarioPagamentoPedidoActivity.java
@@ -1,11 +1,11 @@
 package com.exemplo.ejle_commerce.activity.usuario;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.exemplo.ejle_commerce.R;
 import com.exemplo.ejle_commerce.dao.ItemDAO;
 import com.exemplo.ejle_commerce.dao.ItemPedidoDAO;
 import com.exemplo.ejle_commerce.databinding.ActivityUsuarioPagamentoPedidoBinding;
@@ -13,11 +13,14 @@ import com.exemplo.ejle_commerce.helper.FirebaseHelper;
 import com.exemplo.ejle_commerce.model.Endereco;
 import com.exemplo.ejle_commerce.model.ItemPedido;
 import com.exemplo.ejle_commerce.model.Loja;
+import com.exemplo.ejle_commerce.model.Produto;
 import com.exemplo.ejle_commerce.model.Usuario;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +47,65 @@ public class UsuarioPagamentoPedidoActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         recuperarDados();
+
+        binding.progressBar.setOnClickListener(v -> {
+            configJSON();
+        });
+    }
+
+    private void configJSON() {
+        JsonObject dados = new JsonObject();
+
+        JsonArray itemsList = new JsonArray();
+
+        JsonObject payer = new JsonObject();
+        JsonObject phone = new JsonObject();
+        JsonObject address = new JsonObject();
+        JsonObject payment_methods = new JsonObject();
+
+        String telefone = usuario.getTelefone()
+                .replace("(", "")
+                .replace(")", "")
+                .replace(" ", "");
+
+        phone.addProperty("area_code", telefone.substring(0, 2));
+        phone.addProperty("number", telefone.substring(2, 12));
+
+        address.addProperty("street_name", enderecoSelecionado.getLogradouro());
+        if(enderecoSelecionado.getNumero() != null) {
+            address.addProperty("street_number", enderecoSelecionado.getNumero());
+        }
+        address.addProperty("zip_code", enderecoSelecionado.getCep());
+
+        payment_methods.addProperty("installments", loja.getParcelas());
+
+        JsonObject item;
+        for (ItemPedido itemPedido : itemPedidoList) {
+            Produto produto = itemPedidoDAO.getProduto(itemPedido.getId());
+
+            item = new JsonObject();
+            item.addProperty("title", produto.getTitulo());
+            item.addProperty("currency_id", "BRL");
+            item.addProperty("picture_url", produto.getUrlsImagens().get(0).getCaminhoImagem());
+            item.addProperty("quantity", itemPedido.getQuantidade());
+            item.addProperty("unit_price", produto.getValorAtual());
+
+            itemsList.add(item);
+
+            itemPedido.setNomeProduto(produto.getTitulo());
+        }
+
+        dados.add("items", itemsList);
+
+        payer.addProperty("name", usuario.getNome());
+        payer.addProperty("email", usuario.getEmail());
+        payer.add("phone", phone);
+        payer.add("address", address);
+
+        dados.add("payer", payer);
+        dados.add("payment_methods", payment_methods);
+
+        Log.i("INFOTESTE", "configJSON: " + dados);
     }
 
     private void recuperarDados() {
